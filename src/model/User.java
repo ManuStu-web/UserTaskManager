ed7ab3914afb0ed14eeaa44a98bc61b673e361cb
Manu Sharma <msharma.stu@gmail.com>
2026-01-25 14:10:58 +0530

    Created User File with Details

---
+++ src/model/User.java
@@ -0,0 +1,56 @@
+package model;
+public class User
+{
+    
+    private int id;         //Unique identifier for the user
+    private String name;     // User's full name
+    private String email;    // User's email address
+    private String password; //Password for user authentication
+
+    public User(int id, String name , String email , String password)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    //Private Data Members so create Getters and Setters (This makes data members safe and secure)
+
+    //Getters
+    public int getId()
+    {
+        return id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+
+    //Setters
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    
+}

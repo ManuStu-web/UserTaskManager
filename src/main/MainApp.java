aa43777f5012187aa1078d46d1a14267af6c276f
Manu Sharma <msharma.stu@gmail.com>
2026-01-26 14:12:06 +0530

    Added DBConnection Utility

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,4 +1,19 @@
 package main;
-public class MainApp {
-    
+import util.DBConnection;
+import java.sql.Connection;
+
+public class MainApp 
+{
+    public static void main(String[] args) {
+        Connection con = DBConnection.getConnection();
+        
+        if(con!=null)
+        {
+            System.out.println("Database connection succesfull");
+        }
+        else
+        {
+            System.out.println("Connection Failed");
+        }
+    }
 }

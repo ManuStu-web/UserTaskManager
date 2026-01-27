7eb43e59bb29ce2aa7a4ae7653a117b36420ca16
Manu Sharma <msharma.stu@gmail.com>
2026-01-27 21:28:46 +0530

    Minute changes

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection
 {
     private static final String URL = "jdbc:mysql://localhost:3306/task_management?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "Password_here";
+    private static final String PASSWORD = "5140M@nu2299";
 
     public static Connection connection;
 
